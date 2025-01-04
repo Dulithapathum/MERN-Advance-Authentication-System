57d1d84a7bd3352c30157fdf7e2533f56dd3e2d9
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2025-01-04 11:28:53 +0530

    Config Cors To Backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import dontenv from "dotenv";
 import { ConnectDB } from "./DB/db.js";
 import router from "./Routes/authRoutes.js";
+
 const app = express();
 app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 dontenv.config();
 
 ConnectDB();
